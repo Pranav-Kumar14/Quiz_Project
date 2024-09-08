898b6e05ef6d1ddaff24b292e668efc7181ebbeb
Pranav Kumar <pranav.sin123@gmail.com>
2024-09-08 15:24:58 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -80,6 +80,7 @@ function showQuestion(){
 
 function resetState(){
     nextButton.style.display = "none";
+    next.button.innerHTML = "Next";
     while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild); 
     }
